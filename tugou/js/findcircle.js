e5c7ad866c23ae06efa263815d1dd1dbbf4b59ef
hh4454 <1130034920@qq.com>
2016-07-22 17:48:15 +0800

    detzhdhfdfhd
    hdfzdfhzdfhzgf

--- tugou/js/findcircle.js
+++ tugou/js/findcircle.js
@@ -1,7 +1,7 @@
 window.onload=function(){
 	//。。。。。。。。。。。。。。。。。。。。。。。收藏
 	var div3 = document.getElementsByClassName('div3');
-	//console.log(div3.length);
+//	console.log(div3.length);
 	for (var i = 0; i < div3.length; i++) {
 		div3[i].onclick=function(){
 			if(this.className != "div31"){
@@ -15,7 +15,7 @@ window.onload=function(){
 	}
 //.................................点赞	
    var ctop = document.getElementsByClassName('ctop1');
-   console.log(ctop.length)
+   //console.log(ctop.length)
    for (var i = 0; i < ctop.length; i++) {
    	ctop[i].onclick=function(){
    		if(this.className!="ctop11"){
@@ -25,6 +25,9 @@ window.onload=function(){
    		}
    	}
    }
+// ...........................................底部  
+//	var 
+   
  
 	
 	
