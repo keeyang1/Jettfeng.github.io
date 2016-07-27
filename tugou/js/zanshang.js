33b2d1d5edb2cb8a2ca8df2386d03090e909ea02
hh4454 <1130034920@qq.com>
2016-07-27 15:08:25 +0800

    sdf
    sdfdsfdfff

--- tugou/js/zanshang.js
+++ tugou/js/zanshang.js
@@ -13,14 +13,15 @@ window.onload=function(){
 	 }
 	
 	function enterFn(){
-		gray.style.display = "block";
-		startMove(gray,'opacity',90,function(){			
-				middle.style.display = "block";
-				var a = middle.offsetHeight
-				alert(a);
-				middle.style.height = 0;
-				startMove(middle,'height',a)
-		})
+		// gray.style.display = "block";
+		// startMove(gray,'opacity',90,function(){			
+		// 		middle.style.display = "block";
+		// 		var a = middle.offsetHeight
+		// 		alert(a);
+		// 		middle.style.height = 0;
+		// 		startMove(middle,'height',a)
+		// })
+		middle.style.display = "block"
 	}
 	
     function outFn(){
